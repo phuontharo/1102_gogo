9af39123b1143b41c39fe78f91f1fa6bb1ffbcf0
phuontharo <hvtelphthphth@gmail.com>
2020-11-02 10:13:46 +0700

    1102_test

--- app/src/main/java/com/example/testimg/MainActivity.java
+++ app/src/main/java/com/example/testimg/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.testimg;
 
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -53,6 +55,40 @@ public class MainActivity extends AppCompatActivity {
         progressBar1.setProgress(100);
         progressBar2.setProgress(100);
         Init();
+    }
+
+    private AnimationDrawable getProgressBarAnimation() {
+        GradientDrawable rainbow1 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+                new int[]{Color.RED, Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW});
+
+        GradientDrawable rainbow2 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+                new int[]{Color.YELLOW, Color.RED, Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN});
+
+        GradientDrawable rainbow3 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+                new int[]{Color.GREEN, Color.YELLOW, Color.RED, Color.MAGENTA, Color.BLUE, Color.CYAN});
+
+        GradientDrawable rainbow4 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+                new int[]{Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED, Color.MAGENTA, Color.BLUE});
+
+        GradientDrawable rainbow5 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+                new int[]{Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED, Color.MAGENTA});
+
+        GradientDrawable rainbow6 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+                new int[]{Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED});
+
+
+        GradientDrawable[] gds = new GradientDrawable[]{rainbow1, rainbow2, rainbow3, rainbow4, rainbow5, rainbow6};
+
+        AnimationDrawable animation = new AnimationDrawable();
+
+        for (GradientDrawable gd : gds) {
+            animation.addFrame(gd, 100);
+
+        }
+
+        animation.setOneShot(false);
+
+        return animation;
     }
 
     void Init() {
